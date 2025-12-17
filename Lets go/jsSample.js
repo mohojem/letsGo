c5e9d69ce9323f00702dd7c5acfc032686f4f660
MPHO MOHOJE <mohoje2mpho@gmail.com>
2025-12-17 15:08:23 +0200

    Add files via upload

---
+++ Lets go/jsSample.js
@@ -0,0 +1,4 @@
+console.log['------------------'];
+console.log['Rise and shine babygirl!'];
+console.log['Super ready for a new day'];
+console.log['------------------']  
---
+++ Lets go/pythonSample.py
@@ -0,0 +1,4 @@
+print['------------------------------------']
+print['Babygirl in tech']
+print['FNB app academy certificate recieved']
+print['------------------------------------'] 
